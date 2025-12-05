6f20ae8839c44f3d9077f5a26d52f655a889b1d3
llamber <llamber@axelera.ee.ethz.ch>
2025-12-05 13:50:35 +0100

    improved script

--- source_5.20.4.sh
+++ source_5.20.4.sh
@@ -1,8 +1,23 @@
 # 5.20.4
-conda activate gap_sdk_5.20.4
-export GAP_RISCV_GCC_TOOLCHAIN="/home/lamberti/gap/5.20.4/gap_riscv_toolchain_ubuntu/install/"
-source /home/lamberti/gap/5.20.4/gap_sdk_private/configs/gap9_evk_audio.sh
-# export GAPY_OPENOCD_CABLE=$HOME/gap/5.20.4/gap_sdk_private/utils/openocd/tcl/interface/ftdi/olimex-arm-usb-ocd-h.cfg
+export SDK_BASE="$HOME/gap"
+export SDK_VERSION="5.20.4"
+export SDK_DIR="$GAP_SDK_BASE/$GAP_SDK_VERSION/"
+
+# Activate conda environment
+conda activate "gap_sdk_$SDK_VERSION"
+#conda activate gap_sdk_5.20.4
+
+# RISC-V toolchain
+export GAP_RISCV_GCC_TOOLCHAIN="$SDK_DIR/gap_riscv_toolchain_ubuntu/install/"
+
+# Source GAP SDK
+source "$SDK_DIR/gap_sdk_private/sourceme.sh"
+
+# Use Olimex (GAP9Shield)
+# export GAPY_OPENOCD_CABLE=$SDK_DIR/gap_sdk_private/utils/openocd/tcl/interface/ftdi/olimex-arm-usb-ocd-h.cfg
+
+# Use FTDI (GAP EVK)
+# export GAPY_OPENOCD_CABLE=$SDK_DIR/gap_sdk_private/utils/openocd_tools/tcl/gapuino_ftdi.cfg
 
 #### how to run an app
 # # Init cmake build directory, named "build"
